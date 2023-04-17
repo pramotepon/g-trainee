ed7f47e750bca76feef4def0b1e95b0b50d1b7c5
ArlifTagaree <liffism9@gmail.com>
2023-04-17 10:19:28 +0700

    Test01

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        EIEI
+        EIEI WOO ZA
       </p>
     </div>
   )
