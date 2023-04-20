2355a1e209c560d88e9149aa4f5a837a60636070
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-04-20 15:21:52 +0700

    Register Page

---
+++ frontend/src/components/ActivityCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ActivityCard = () => {
+  return (
+    <div class="history-card">                                 
+                <img src="#" alt="" class = "idol" />
+                <div>
+                    <h2>Title</h2>
+                    <p>Description</p>
+                </div>
+            </div>
+  )
+}
+
+export default ActivityCard
